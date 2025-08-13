8b9900c756d0a0fbb5c9c50f6d1e841f79ba1823
MAHIN951NB <tyuoghk681@gmail.com>
2025-08-13 04:15:13 -0700

    chaining

--- method chaining/method.js
+++ method chaining/method.js
@@ -1,1 +1,8 @@
-let user = 'md eftikhar'
+let user = 'md eftikhar';
+
+let result1 = user.charAt(0).toUpperCase() + 
+user.slice(1,user.indexOf(" "));
+let result2 = user.charAt(user.indexOf(" ")+1).toUpperCase() + 
+user.slice(user.indexOf(" ")+2);
+let final = result1+" "+result2;
+console.log(final);
