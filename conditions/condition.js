dd7e20347e940bb6eed2c42430d26822c1933024
MAHIN951NB <tyuoghk681@gmail.com>
2025-08-13 03:15:27 -0700

    conditions

---
+++ conditions/condition.js
@@ -0,0 +1,20 @@
+let student,age,mother,father,id,school;
+student = `mahi`;
+father= 'mahbub';
+mother='hasina';
+id='22-47635-2';
+school='AIUB';
+if(student === "mhi"){
+    if(father === "mahbub" && mother ==='hasina'){
+        console.log(`he is absolutely your child`);
+    }
+    else{
+        console.log(`maybe he is not your son`);
+    }
+}
+else if(id === '22-47635--2' || school === 'AIUB'){
+    console.log(`he is your child`);
+}
+else{
+    console.log('couldntidentify');
+}
