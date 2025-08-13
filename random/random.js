bf95a1741da9d39b8d36b9faf71af82dfa84d25a
MAHIN951NB <tyuoghk681@gmail.com>
2025-08-13 02:46:59 -0700

    random

---
+++ random/random.js
@@ -0,0 +1,34 @@
+let maths = Math.random() * 6;
+//console.log(maths);
+
+//let maths2 = Math.round(Math.random()*10)+1;
+//console.log(maths2);
+let min,max;
+min=5;
+max=10;
+let maths2 = Math.round(Math.random()*(max-min))+min;;
+//console.log(maths2);
+
+let x =2;
+let y=3;
+let z=3.4;
+let z2=3.578;
+let k=9;
+
+/*console.log(Math.ceil(z));
+console.log(Math.floor(z));
+console.log(Math.trunc(z));
+console.log(Math.round(z2));
+console.log(Math.round(z));
+console.log(Math.PI);
+console.log(Math.max(x,y));
+console.log(Math.min(x,y));
+console.log(Math.pow(x,y));
+console.log(Math.sqrt(k));*/
+
+for(let i=0; i<10;i++){
+    let maths = Math.random() * 6;
+    console.log(maths);
+}
+
+
