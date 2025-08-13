801ca4ebc2b0dad2044674b32612745d7df814b3
MAHIN951NB <tyuoghk681@gmail.com>
2025-08-13 03:02:22 -0700

    switch

---
+++ switch/switch.js
@@ -0,0 +1,27 @@
+var day = `friday`;
+var day = `sunday`
+
+switch(day){
+    case `monday`:
+        console.log(`today ismonday`);
+        break;
+    case `sunday`:
+        console.log(`today is sunday its off`);
+        break;
+    default:
+    console.log(`nothing founded`)        
+}
+
+var obtained = 85;
+switch(true){
+    case obtained >= 84:
+        console.log(`your grade is B+`);
+        break;
+    case obtained <= 89:
+        console.log(`Your grade is A`); 
+        break;
+    case obtained <= 90:
+        console.log(`your grade is a+ bravo!!`);
+        break    
+
+}
